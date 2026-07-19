334fc6d374f62bb9d44b996d1959eeac5a5ce76c
virgiliovalente <virgilio.valente@kwportugal.pt>
2026-07-19 19:29:01 +0100

    Update main.js

--- css/js/main.js
+++ css/js/main.js
@@ -1,1 +1,30 @@
-// VirgilioValente.pt
+window.addEventListener("load", () => {
+
+    const name = document.getElementById("name");
+    const subtitle = document.getElementById("subtitle");
+    const line1 = document.getElementById("line1");
+    const line2 = document.getElementById("line2");
+    const scroll = document.querySelector(".scroll");
+
+    setTimeout(() => {
+        name.classList.add("fade");
+    }, 2000);
+
+    setTimeout(() => {
+        subtitle.classList.add("fade");
+    }, 4000);
+
+    setTimeout(() => {
+        line1.classList.add("fade");
+    }, 6000);
+
+    setTimeout(() => {
+        line2.classList.add("fade");
+    }, 7000);
+
+    setTimeout(() => {
+        scroll.classList.add("fade");
+        scroll.style.animation += ", bounce 2s infinite";
+    }, 9000);
+
+});
